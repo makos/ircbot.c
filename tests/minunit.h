7c2fed4607faab3b39a7d5e1296c02791a8fc6ed
Mateusz Makowski <matmakos@gmail.com>
2018-09-14 21:22:29 +0200

    Flesh out test suite

--- tests/minunit.h
+++ tests/minunit.h
@@ -1,18 +1,29 @@
 #ifndef IRCBOT_TESTS_MINUNIT_H_
 #define IRCBOT_TESTS_MINUNIT_H_
 
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define YELLOW "\033[0;33m"
+#define COLOR_END "\033[0m"
+
 #define mu_assert(message, test)                                               \
     do {                                                                       \
         if (!(test))                                                           \
             return message;                                                    \
     } while (0)
-#define mu_run_test(test)                                                      \
+#define mu_run_test(name, test)                                                \
     do {                                                                       \
+        printf(YELLOW "TEST: %s\n" COLOR_END, name);                           \
         char *message = test();                                                \
         tests_run++;                                                           \
-        if (message)                                                           \
-            return message;                                                    \
+        if (message) {                                                         \
+            strcpy(messages[tests_failed], message);                           \
+            tests_failed++;                                                    \
+        }                                                                      \
     } while (0)
+
 extern int tests_run;
+extern int tests_failed;
+extern char **messages;
 
 #endif // IRCBOT_TESTS_MINUNIT_H_
