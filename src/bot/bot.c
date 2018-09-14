48da1d9d683dbd8f43dc91004193a83305a1fcc6
Mateusz Makowski <matmakos@gmail.com>
2018-09-14 21:22:06 +0200

    Add empty bot.h/.c

--- src/bot/bot.c
+++ src/bot/bot.c
@@ -1,36 +1,8 @@
 #include "bot.h"
-#include "client/client.h"
-#include "server/irc_server.h"
-#include <stdlib.h>
-#include <string.h>
+#include "connection/connection.h"
+#define NULL ((void *)0)
 
-static void bot_init_channels(IRC_Bot *bot)
+IRC_Bot *bot_create(const char nick[])
 {
-    bot->channel_list = malloc(sizeof(Bot_Channels));
-    bot->channel_list->prev = NULL;
-    bot->channel_list->next = NULL;
-    bot->channel_list->channel_name = NULL;
-}
-
-static void bot_init_commands(IRC_Bot *bot)
-{
-    bot->commands = malloc(sizeof(Bot_Commands));
-    bot->commands->pong = malloc(5 * sizeof(char));
-    bot->commands->version = malloc(16 * sizeof(char));
-    bot->commands->whois = malloc(52 * sizeof(char));
-    strcpy(bot->commands->pong, "PONG");
-    strcpy(bot->commands->version, "ircbot.c v0.1.0");
-    strcpy(bot->commands->whois,
-           "ircbot.c v0.1.0 by makos <github.com/makos/ircbot.c");
-}
-
-IRC_Bot *bot_create(const char address[], const char port[],
-                    const char bot_nick[])
-{
-    IRC_Bot *temp = malloc(sizeof(IRC_Bot));
-    bot_init_channels(temp);
-    bot_init_commands(temp);
-
-    temp->client = client_create();
-    IRC_Server *server = server_create(server, port);
+    return NULL;
 }
--- src/bot/bot.h
+++ src/bot/bot.h
@@ -1,28 +1,9 @@
 #ifndef IRCBOT_BOT_BOT_H_
 #define IRCBOT_BOT_BOT_H_
-typedef struct IRC_Client IRC_Client;
-
-typedef struct Bot_Channel {
-    struct Bot_Channel *prev;
-    char *channel_name;
-    struct Bot_Channel *next;
-} Bot_Channels;
-
-typedef struct Bot_Commands {
-    char *pong;
-    char *version;
-    char *whois;
-} Bot_Commands;
 
 typedef struct IRC_Bot {
-    IRC_Client *client;
-    Bot_Channels *channel_list;
-    Bot_Commands *commands;
-    char *nick;
 } IRC_Bot;
 
-IRC_Bot *bot_create(const char address[], const char port[],
-                    const char bot_nick[]);
-void bot_parse_commands(IRC_Bot *bot);
+IRC_Bot *bot_create(const char nick[]);
 
 #endif // IRCBOT_BOT_BOT_H_
