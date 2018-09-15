f4b882e0da6779b3814f62d5da6481318e417c3e
Mateusz Makowski <matmakos@gmail.com>
2018-09-15 10:37:12 +0200

    Add keepalive connection test

---
+++ tests/test_connection_keepalive.c
@@ -0,0 +1,51 @@
+#include "connection/connection.h"
+#include "minunit.h"
+#include <stdlib.h>
+#include <string.h>
+
+int PINGS_RECEIVED = 0;
+
+static void pong(IRC_Connection *connection)
+{
+    char *id = strtok(connection->recvbuf, ":");
+    id = strtok(NULL, ":");
+    char *pong_msg = malloc(32 * sizeof(char));
+    strcpy(pong_msg, "PONG ");
+    strcat(pong_msg, id);
+    connection_send(connection, pong_msg);
+}
+
+static void parse(IRC_Connection *connection)
+{
+    // if (strstr(connection->recvbuf, "PING") != NULL) {
+    if (strncmp(connection->recvbuf, "PING", (size_t)4) == 0) {
+        PINGS_RECEIVED++;
+        pong(connection);
+    }
+}
+
+static char *test_connection_keepalive()
+{
+    IRC_Connection *temp = connection_create("irc.rizon.net", "6660");
+    if (connection_connect(temp) == 0) {
+        char *errmsg = "TEST: connection_connect(): error";
+        return errmsg;
+    }
+
+    connection_send(temp, "USER ircbot ircbot ircbot ircbot");
+    connection_send(temp, "NICK ircbot");
+
+    int bytes = 0;
+    do {
+        bytes = connection_read(temp);
+
+        for (int i = 0; i < bytes; i++) {
+            printf("%c", temp->recvbuf[i]);
+        }
+
+        parse(temp);
+    } while (bytes > 0 && PINGS_RECEIVED < 4);
+
+    connection_disconnect(temp);
+    return 0;
+}
