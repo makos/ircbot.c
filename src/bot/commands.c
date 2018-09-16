5b6ae0fce7e4d7c88ace76150242c97fe7c856c6
Mateusz Makowski <matmakos@gmail.com>
2018-09-16 21:05:40 +0200

    Add commands.h

---
+++ src/bot/commands.c
@@ -0,0 +1,16 @@
+/**
+ * @file commands.c
+ * @author Mateusz Makowski
+ * @date 16.09.2018
+ * @brief Implementation of commands.h.
+ */
+#include "commands.h"
+#include "bot.h"
+
+#define OK 1
+#define ERROR 0
+
+int commands_ident(IRC_Bot *bot)
+{
+    return ERROR;
+}
---
+++ src/bot/commands.h
@@ -0,0 +1,17 @@
+/**
+ * @file commands.h
+ * @author Mateusz Makowski
+ * @date 16.09.2018
+ * @brief File containing bot command functions.
+ */
+#ifndef IRCBOT_BOT_COMMANDS_H_
+#define IRCBOT_BOT_COMMANDS_H_
+struct IRC_Bot;
+/**
+ * Send an ident response to the server.
+ * @param bot pointer to IRC_Bot structure
+ * @return 0 on failure, 1 on success
+ */
+int commands_ident(struct IRC_Bot *bot);
+
+#endif // IRCBOT_BOT_COMMANDS_H_
