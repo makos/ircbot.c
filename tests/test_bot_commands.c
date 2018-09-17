b13c35040c005d90a5f591bef229f19b0f762b86
Mateusz Makowski <matmakos@gmail.com>
2018-09-17 15:28:10 +0200

    Style

--- tests/test_bot_commands.c
+++ tests/test_bot_commands.c
@@ -7,11 +7,15 @@ static char *test_bot_ident_success()
 {
     IRC_Bot *test_bot = bot_create("ircbot");
     mu_assert("test_bot_ident_success(): test_bot is NULL", test_bot);
+
     bot_add_command(test_bot, "ident", commands_ident);
+
     bot_connect(test_bot, "irc.rizon.net", "6660");
+
     mu_assert_or_disconnect(
         test_bot, "test_bot_ident_success(): bot_call() returned ERROR",
         bot_call(test_bot, "ident") != 0);
+
     bot_disconnect(test_bot);
     return 0;
 }
