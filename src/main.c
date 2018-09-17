9cbff4ebba97cf505237d8799738b9bc04a3ad4c
Mateusz Makowski <matmakos@gmail.com>
2018-09-17 14:00:27 +0200

    Update mainpage

--- src/main.c
+++ src/main.c
@@ -3,31 +3,17 @@
  *
  * @section index_about About
  * This is a cross-platform (Linux and Windows tested) IRC bot written purely in
- * C without third-party libraries.
+ * C without third-party libraries. Very work-in-progress.
  *
- * @section index_req Requirements
- * For building from source: (all platforms)
- *  - CMake (version >= 3.0.0)
- *  - gcc
- *  - preferably GNU make
+ * @section index_ideas To-do list
+ * @todo Command line for the bot (probably implemented via ncurses, for sake of
+ * simplicity)
+ * @todo Public API with common functions like: send, read, join, leave etc.
+ * @todo Logging
  *
- * @section index_building Building
- * Navigate to the folder with source code (where README.md is located). Then:
- * ````
- * mkdir build && cd build
- * cmake .. && make tests
- * tests/tests all
- * ````
- * As of version 0.2.0 only tests will compile and run.
- */
-
-/* TODO: list of things I'd like to have:
- * command-line for the bot (send message, whisper, join/leave channels)
- * UTF-8 printing
- * cross-platform
- * multi-channel capability
- * automatic response to common IRC queries
- * some ice cream
+ * @section Public API
+ * For starters, take a look at the IRC_Bot structure. This describes the public
+ * API.
  */
 
 int main(int argc, char const *argv[])
