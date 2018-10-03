b4e41068f5f3a39858007bba7e2e9cbdc0266c92
Mateusz Makowski <matmakos@gmail.com>
2018-10-03 15:09:38 +0200

    Fix chatterbot

--- chatter/main.c
+++ chatter/main.c
@@ -1,10 +1,10 @@
-#include <string.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include "bot.h"
 #include "commands.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-static void parse_input(IRC_Bot* bot)
+static void parse_input(IRC_Bot *bot)
 {
     if (strncmp(bot->last_msg, "PING", 4) == 0) {
         commands_pong(bot);
@@ -15,31 +15,28 @@ static void parse_input(IRC_Bot* bot)
 // This segfaults???
 int main()
 {
-    char *channels[] = {
-        "#dailyprog",
-        "#ircbot_ctest"
-    };
+    char *channels[] = {"#dailyprog", "#ircbot_ctest"};
 
     IRC_Bot *chatter = bot_create("chatterbot");
-    /* bot_connect(chatter, "irc.rizon.net", "6660"); */
+    bot_connect(chatter, "irc.rizon.net", "6660");
 
-    /* bot_send(chatter, "USER chatterbot chatterbot chatterbot chatterbot"); */
-    /* bot_send(chatter, "NICK chatterbot"); */
+    bot_send(chatter, "USER chatterbot chatterbot chatterbot chatterbot");
+    bot_send(chatter, "NICK chatterbot");
 
     int joined = 0;
-    /* while (1) { */
-    /*     bot_read(chatter); */
+    while (1) {
+        bot_read(chatter);
 
-    /*     printf("%s", chatter->last_msg); */
+        printf("%s", chatter->last_msg);
 
-    /*     parse_input(chatter); */
+        parse_input(chatter);
 
-    /*     /\* if (!joined) { *\/ */
-    /*     /\*     bot_join(chatter, "#ircbot_ctest"); *\/ */
-    /*     /\*     joined = 1; *\/ */
-    /*     /\* } *\/ */
-    /* } */
+        /*     /\* if (!joined) { *\/ */
+        /*     /\*     bot_join(chatter, "#ircbot_ctest"); *\/ */
+        /*     /\*     joined = 1; *\/ */
+        /*     /\* } *\/ */
+    }
 
-    /* bot_disconnect(chatter); */
+    bot_disconnect(chatter);
     return 0;
 }
