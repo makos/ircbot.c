3caa40ba3426e72dbefab644da78fca8861f897f
Mateusz Makowski <matmakos@gmail.com>
2020-01-06 21:16:00 +0100

    WIP of parsing IRC messages with RFC 2812

--- chatter/main.c
+++ chatter/main.c
@@ -1,18 +1,43 @@
 #include "bot.h"
 #include "commands.h"
+#include "chatter.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static void parse_input(IRC_Bot *bot)
+#define TRUE 1
+#define FALSE 0
+
+static void parse_incoming_data(IRC_Bot *bot)
 {
-    if (strncmp(bot->last_msg, "PING", 4) == 0) {
-        commands_pong(bot);
-    } else if (strncmp(bot->last_msg, "Hello bot", 10) == 0) {
-        bot_send(bot, "MSG #ircbot_ctest Hello");
+    // http://www.networksorcery.com/enp/protocol/irc.htm
+    // "The presence of a prefix is indicated with a single leading ASCII colon
+    // character", hence this check. 
+    int has_prefix = FALSE;
+    if (strncmp(bot->last_msg, ":", 1) == 0)
+        has_prefix = TRUE;
+    
+    Message *incoming_msg = malloc(sizeof(Message));
+
+    if (has_prefix) {
+        int prefix_delimiter_offset = 1; // This is always 1; maybe should be a constant?
+        int only_servername = FALSE;
+        int prefix_len = strcspn(bot->last_msg, " ");
+        int nickname_len = strcspn(bot->last_msg, "!");
+        // If position of "!" character is before the total length of prefix,
+        // we have full "nick!user@host" prefix. Otherwise, it's only a server
+        // address (e.g. "irc.rizon.no"). If strcspn() doesn't find a match,
+        // it returns the full string length (equivalent to strlen()).
+        if (nickname_len > prefix_len) {
+            strncpy(incoming_msg->servername, bot->last_msg + prefix_delimiter_offset, prefix_len - prefix_delimiter_offset);
+            // DEBUG
+            printf("DEBUG: incoming_msg->servername: |%s|\n", incoming_msg->servername);
+        } else {
+            int user_len = strcspn(bot->last_msg, "@");
+        }
     }
 }
-// This segfaults??? - not on Windows lol
+
 int main()
 {
     char *channels[] = {"#dailyprog", "#ircbot_ctest"};
@@ -29,12 +54,12 @@ int main()
 
         printf("%s", chatter->last_msg);
 
-        parse_input(chatter);
+        parse_incoming_data(chatter);
 
-        /*     /\* if (!joined) { *\/ */
-        /*     /\*     bot_join(chatter, "#ircbot_ctest"); *\/ */
-        /*     /\*     joined = 1; *\/ */
-        /*     /\* } *\/ */
+        // if (!joined) {
+        //     bot_join(chatter, "#ircbot_ctest");
+        //     joined = 1;
+        // }
     }
 
     bot_disconnect(chatter);
