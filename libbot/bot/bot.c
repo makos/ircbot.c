a468eba8cd9d9e857226bda618ac2e7ab52f7c2a
Mateusz Makowski <matmakos@gmail.com>
2020-01-06 21:16:51 +0100

    Wrong initialization of IRC_Bot->channels
    Commented out for now, it led to heap errors; only with MSVC tho

--- libbot/bot/bot.c
+++ libbot/bot/bot.c
@@ -48,11 +48,11 @@ IRC_Bot *bot_create(const char nick[])
     temp->nick = malloc((strlen(nick) + 1) * sizeof(char));
     strcpy(temp->nick, nick);
 
-    temp->channels = malloc(BOT_MAX_CHANNEL_AMT);
-    for (int i = 0; i < BOT_MAX_CHANNEL_AMT; i++) {
-        temp->channels[i] = malloc(BOT_MAX_CHANNEL_LEN * sizeof(char));
-        strcpy(temp->channels[i], "");
-    }
+    // temp->channels = malloc(BOT_MAX_CHANNEL_AMT);
+    // for (int i = 0; i < BOT_MAX_CHANNEL_AMT; i++) {
+    //     temp->channels[i] = malloc(BOT_MAX_CHANNEL_LEN * sizeof(char));
+    //     strcpy(temp->channels[i], "");
+    // }
 
     temp->last_channel_id = -1;
 
@@ -112,29 +112,29 @@ int bot_read(IRC_Bot *bot)
     return bot_recv(bot);
 }
 
-int bot_join(IRC_Bot *bot, const char channel[])
-{
-    if (!bot || !bot->connection) {
-        fprintf(stderr, "bot_join(): bot or connection are not initialized\n");
-        return ERROR;
-    }
+// int bot_join(IRC_Bot *bot, const char channel[])
+// {
+//     if (!bot || !bot->connection) {
+//         fprintf(stderr, "bot_join(): bot or connection are not initialized\n");
+//         return ERROR;
+//     }
 
-    bot->last_channel_id++;
+//     bot->last_channel_id++;
 
-    strcpy(bot->channels[bot->last_channel_id], channel);
+//     strcpy(bot->channels[bot->last_channel_id], channel);
 
-    // +5 chars because of "JOIN "
-    char *join_msg = malloc((BOT_MAX_CHANNEL_LEN + 5) * sizeof(char));
-    strcpy(join_msg, "JOIN ");
-    strcat(join_msg, channel);
+//     // +5 chars because of "JOIN "
+//     char *join_msg = malloc((BOT_MAX_CHANNEL_LEN + 5) * sizeof(char));
+//     strcpy(join_msg, "JOIN ");
+//     strcat(join_msg, channel);
 
-    if (bot_send(bot, join_msg) == ERROR) {
-        fprintf(stderr, "bot_join(): bot_send() returned ERROR\n");
-        return ERROR;
-    }
+//     if (bot_send(bot, join_msg) == ERROR) {
+//         fprintf(stderr, "bot_join(): bot_send() returned ERROR\n");
+//         return ERROR;
+//     }
 
-    return OK;
-}
+//     return OK;
+// }
 
 int bot_leave(IRC_Bot *bot, const char channel[])
 {
