ee24a8d14d61c8a0e184009b835c89fc497db7a2
Mateusz Makowski <matmakos@gmail.com>
2020-01-08 20:12:52 +0100

    Add servername only message for testing

--- tests/server/test_server.c
+++ tests/server/test_server.c
@@ -52,7 +52,9 @@ int server_read(Mock_Server *server)
 {
     int sockaddr_size = sizeof(struct sockaddr_in);
     char hello_msg[] = ":nick!user@host MSG hello\n";
+    char server_msg[] = ":server.lol NOTICE hello\n";
     int hello_msg_len = strlen(hello_msg);
+    int server_msg_len = strlen(server_msg);
 
     int bytes = 0;
     while ((server->socket_client =
@@ -62,6 +64,10 @@ int server_read(Mock_Server *server)
         fcntl(server->socket_client, F_SETFD, O_NONBLOCK);
 
         if (send(server->socket_client, hello_msg, hello_msg_len, 0) == -1) {
+            fprintf(stderr, "%d %s", errno, strerror(errno));
+        }
+
+        if (send(server->socket_client, server_msg, server_msg_len, 0) == -1) {
             fprintf(stderr, "%d %s", errno, strerror(errno));
         }
 
