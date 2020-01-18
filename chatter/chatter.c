94c03fa40418531dfda8b69b01ce3b88b6fab20b
Mateusz Makowski <matmakos@gmail.com>
2020-01-18 23:43:34 +0100

    Clean up chatter.c

--- chatter/chatter.c
+++ chatter/chatter.c
@@ -37,8 +37,6 @@ static void parse_incoming_data(IRC_Bot *bot)
         if (nick_end_index > prefix_len) {
             tok = strtok(incoming_msg->msg_copy + PREFIX_DELIMITER_OFFSET, " ");
             strcpy(incoming_msg->servername, tok);
-            //strcpy(incoming_msg->servername,
-                   //get_prefix_chunk(bot->last_msg, ":", " "));
             // DEBUG:
             printf("DEBUG incoming_msg->servername: |%s|\n",
                     incoming_msg->servername);
@@ -53,13 +51,6 @@ static void parse_incoming_data(IRC_Bot *bot)
 
             tok = strtok(NULL, " ");
             strcpy(incoming_msg->host, tok);
-            //strcpy(incoming_msg->nickname,
-                   //get_prefix_chunk(bot->last_msg, ":", "!"));
-            //strcpy(incoming_msg->user,
-                   //get_prefix_chunk(bot->last_msg, "!", "@"));
-            //strcpy(incoming_msg->host,
-                   //get_prefix_chunk(bot->last_msg, "@", " "));
-
             // DEBUG:
             printf("DEBUG incoming_msg: |%s| |%s| |%s|\n",
                   incoming_msg->nickname, incoming_msg->user,
@@ -68,18 +59,8 @@ static void parse_incoming_data(IRC_Bot *bot)
         // Now let's get the command and its parameters.
         tok = strtok(NULL, " ");
         strcpy(incoming_msg->command, tok);
-        //strcpy(incoming_msg->command,
-               //get_prefix_chunk(bot->last_msg, " ", " "));
         // DEBUG:
         //printf("DEBUG command: |%s|\n", incoming_msg->command);
-       
-        // Grab a copy of the message for strtok().
-        //char msg_copy[MAX_ARRAY_LEN];
-        //strcpy(msg_copy, bot->last_msg);
-
-        //char *tok = strtok(
-                //msg_copy + prefix_len + strlen(incoming_msg->command) + 1,
-                //" ");
         
         for (int i = 0; i < MAX_ARRAY_LEN; i++) {
             incoming_msg->parameters[i] = '\0'; 
@@ -93,24 +74,6 @@ static void parse_incoming_data(IRC_Bot *bot)
 
         strcpy(incoming_msg->trailing, tok);
         
-        //while (tok) {
-            //// DEBUG
-            ////printf("DEBUG tok: %s\n", tok);
-            //if (strncmp(tok, ":", 1) != 0) {
-                //strcat(incoming_msg->parameters, tok);
-                //strcat(incoming_msg->parameters, " ");
-                //tok = strtok(NULL, " ");
-            //} else {
-                //// Above code adds a space after every parameter, we don't
-                //// want it after the last one.
-                //int param_len = strlen(incoming_msg->parameters);
-                //incoming_msg->parameters[param_len - 1] = '\0';
-                //tok = NULL;
-            //}
-        //}
-        
-        //strcpy(incoming_msg->trailing,
-               //get_prefix_chunk(bot->last_msg + prefix_len, ":", "\r\n"));
         printf("[%s]: %s\r\n", incoming_msg->nickname, incoming_msg->trailing);
         // DEBUG:
         // printf("DEBUG parameters: |%s|\n", incoming_msg->parameters);
@@ -126,7 +89,7 @@ static void parse_incoming_data(IRC_Bot *bot)
 
 int main()
 {
-    char *channels[] = {"#dailyprog", "#ircbot_ctest"};
+    //char *channels[] = {"#dailyprog", "#ircbot_ctest"};
     ChatterStatus *status = malloc(sizeof(ChatterStatus));
     status->connected = FALSE;
     status->motd_finished = FALSE;
